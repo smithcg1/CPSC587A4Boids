f0dcedd7348dd7465fa775f4de4f1dea008cb669
Christopher <smithcg@ucalgary.ca>
2019-05-08 10:21:31 -0600

    Including built version of file

---
+++ build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/glad.c" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLM_FORCE_CXX14=1"
+  "IMGUI_IMPL_OPENGL_LOADER_CUSTOM=\"glad/glad.h\""
+  "_USE_MATH_DEFINES=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/src"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/glfw/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/curve_gallery.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/curve_gallery.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/givr.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/givr.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/imgui/imgui.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/imgui/imgui.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/imgui/imgui_demo.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/imgui/imgui_demo.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/imgui/imgui_draw.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/imgui/imgui_draw.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/imgui/imgui_impl_glfw.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/imgui/imgui_impl_glfw.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/imgui/imgui_impl_opengl3.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/imgui/imgui_impl_opengl3.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/imgui/imgui_widgets.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/libs/imgui/imgui_widgets.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/src/Boid.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/src/Boid.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/src/main.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/src/main.cpp.o"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/src/panel.cpp" "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/src/panel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLM_FORCE_CXX14=1"
+  "IMGUI_IMPL_OPENGL_LOADER_CUSTOM=\"glad/glad.h\""
+  "_USE_MATH_DEFINES=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/src"
+  "/home/uga/smithcg/Desktop/test/CPSC587A4Boids/libs/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/uga/smithcg/Desktop/test/build-CPSC587A4Boids-Desktop-Default/libs/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-CPSC587A4Boids-Desktop-Default/CMakeFiles/simple.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple.dir/libs/curve_gallery.cpp.o"
+  "CMakeFiles/simple.dir/libs/givr.cpp.o"
+  "CMakeFiles/simple.dir/libs/glad.c.o"
+  "CMakeFiles/simple.dir/libs/imgui/imgui.cpp.o"
+  "CMakeFiles/simple.dir/libs/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/simple.dir/libs/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/simple.dir/libs/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/simple.dir/libs/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/simple.dir/libs/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/simple.dir/src/Boid.cpp.o"
+  "CMakeFiles/simple.dir/src/main.cpp.o"
+  "CMakeFiles/simple.dir/src/panel.cpp.o"
+  "simple.pdb"
+  "simple"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/simple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
